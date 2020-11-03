f7e8c0019307a980017e958aa478e31fc536ed11
Aoibhe1998 <aoibhe.nithuineir-eanaigh@ucdconnect.ie>
2020-11-03 15:27:44 +0000

    Added floodCard method for changing the status of tiles

--- Forbidden-Island-Project/src/Cards/FloodCard.java
+++ Forbidden-Island-Project/src/Cards/FloodCard.java
@@ -1,5 +1,26 @@
 package Cards;
 
-public class FloodCard {
+import Board.*;
 
+public class FloodCard extends Cards {
+	
+	//------------------------------ CONSTRUCTORS ---------------------------------//
+	
+	protected FloodCard(String name) {
+		super(name);
+	}
+	
+	//------------------------------ METHODS --------------------------------------//
+	
+	// turn the tile indicated on the card over if unflooded to the flooded side
+	protected void floodCardChosen(Tile tile) {
+		switch(tile.getTileStatus()) {
+		case UNFLOODED:
+			tile.setTileStatus(FLOODED); // !!! unsure why there is an error here !!!
+		case FLOODED:
+			tile.setTileStatus(SUNK); // !!! unsure why there is an error here !!!
+		default:
+			System.out.println("Something went wrong, please try again!");
+		}	
+	}
 }
