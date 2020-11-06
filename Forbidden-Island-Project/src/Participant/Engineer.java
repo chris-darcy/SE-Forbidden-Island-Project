c6543055b7f22b4295443fc006966bfb4779c314
Aoibhe1998 <aoibhe.nithuineir-eanaigh@ucdconnect.ie>
2020-11-06 12:15:02 +0000

    Created Engineer and made minor change to Participant

---
+++ Forbidden-Island-Project/src/Participant/Engineer.java
@@ -0,0 +1,8 @@
+package Participant;
+
+public class Engineer extends Participant{
+
+	protected Engineer(String name, Hand hand, int location, int actionsRemaining) {
+		super(name, hand, location, actionsRemaining);
+	}
+}
