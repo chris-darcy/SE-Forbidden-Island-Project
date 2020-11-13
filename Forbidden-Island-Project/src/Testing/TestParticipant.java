356b38ac134675e19a1267f20720164fd77b79a7
Aoibhe1998 <aoibhe.nithuineir-eanaigh@ucdconnect.ie>
2020-11-13 09:16:13 +0000

    Commenting out the test class as it is not ready

--- Forbidden-Island-Project/src/Testing/TestParticipant.java
+++ Forbidden-Island-Project/src/Testing/TestParticipant.java
@@ -9,19 +9,19 @@ import Participant.Participant;
 import Participant.Participant.*;
 
 public class TestParticipant {
-	protected List<Card> handNewParticipant = new ArrayList<Card>(); 
-	handNewParticipant.add();
-	handNewParticipant.add();
-	handNewParticipant.add();
-	Hand hand = new Hand(handNewParticipant);
-	Participant newParticipant = new Participant("Bob", "Engineer", hand, 13, 5);
-	
-	protected TestParticipant() {
-		
-	}
-	
-	// test initialising the participant
-	protected void initialiseParticipant() {
-		
-	}
+//	protected List<Card> handNewParticipant = new ArrayList<Card>(); 
+//	handNewParticipant.add();
+//	handNewParticipant.add();
+//	handNewParticipant.add();
+//	Hand hand = new Hand(handNewParticipant);
+//	Participant newParticipant = new Participant("Bob", "Engineer", hand, 13, 5);
+//	
+//	protected TestParticipant() {
+//		
+//	}
+//	
+//	// test initialising the participant
+//	protected void initialiseParticipant() {
+//		
+//	}
 }
