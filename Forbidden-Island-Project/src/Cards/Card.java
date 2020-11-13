241da2e74e54d147b4c513bbc9c42b014f5a1476
Aoibhe1998 <aoibhe.nithuineir-eanaigh@ucdconnect.ie>
2020-11-13 11:33:29 +0000

    CardDeck updates

--- Forbidden-Island-Project/src/Cards/Card.java
+++ Forbidden-Island-Project/src/Cards/Card.java
@@ -2,14 +2,17 @@ package Cards;
 /**
  * Creating an abstract class for TreasureCard and FloodCard to extend from as they has different methods
  */
-public abstract class Card {
+public class Card {
 	protected String name;
+	private CardType cardType;
 	
 	protected Card (String name) {
 		this.name = name;
+//		this.cardType = cardType;
 	}
 	
 	private String getCardName() {
-		return this.name;
+		return name;
 	}
+
 }
--- Forbidden-Island-Project/src/Cards/CardDeck.java
+++ Forbidden-Island-Project/src/Cards/CardDeck.java
@@ -8,73 +8,69 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 
+import Cards.Card.*;
+import Board.TileStatus;
+
 public class CardDeck {
 	
 	public CardDeck() {
+		initialise(28); // 28 cards in Treasure Deck
 	}
 	
-	ArrayList<Card> cardDeck;
+	public ArrayList<TreasureCard> cardDeck = new ArrayList<TreasureCard>();
 	
 	public void initialise(int numberOfCardsInDeck) {
 		String line;
 		String[] attributes;
 		ArrayList<Integer> nums = new ArrayList<Integer>();
-		
-		// Randomize Card indices
-		for(int i=0;i<numberOfCardsInDeck;i++){ // number of cards depends on the deck made
-			nums.add(i);
-		}
-		
-		Collections.shuffle(nums); // shuffle cards
-		Iterator<Integer> idx = nums.iterator();
+		CardType cardType;
 		
 		BufferedReader scan;
 		try {
 			scan = new BufferedReader(new FileReader("src/CardNames.txt"));
 			for (int i = 0; i < numberOfCardsInDeck; i++) {    	
 		    	try {
-					
 		    		line = scan.readLine();
 					attributes = line.split("-"); //Delimiter is a hyphen
 			    	
 			    	switch(attributes[1]) {
 			    		case "F":
-			    			FireTreasureCard fireTreasureCard = new FireTreasureCard(attributes[0]);
+			    			TreasureCard fireTreasureCard = new TreasureCard(attributes[0]);
 			    			cardDeck.add(fireTreasureCard);
 			    			break;
 			    		case "O":
-			    			OceanTreasureCard oceanTreasureCard = new OceanTreasureCard(attributes[0]);
+			    			TreasureCard oceanTreasureCard = new TreasureCard(attributes[0]);
 			    			cardDeck.add(oceanTreasureCard);
 			    			break;
 			    		case "E":
-			    			EarthTreasureCard earthTreasureCard = new EarthTreasureCard(attributes[0]);
+			    			TreasureCard earthTreasureCard = new TreasureCard(attributes[0]);
 			    			cardDeck.add(earthTreasureCard);
 			    			break;
 			    		case "W":
-			    			WindTreasureCard windTreasureCard = new WindTreasureCard(attributes[0]);
+			    			TreasureCard windTreasureCard = new TreasureCard(attributes[0]);
 			    			cardDeck.add(windTreasureCard);
 			    			break;
 			    		case "H":
-			    			HelicopterTreasureCard helicopterTreasureCard = new HelicopterTreasureCard(attributes[0]);
+			    			TreasureCard helicopterTreasureCard = new HelicopterTreasureCard(attributes[0]);
 			    			cardDeck.add(helicopterTreasureCard);
 			    			break;
 			    		case "S":
-			    			SandbagTreasureCard sandbagTreasureCard = new SandbagTreasureCard(attributes[0]);
+			    			TreasureCard sandbagTreasureCard = new SandbagTreasureCard(attributes[0]);
 			    			cardDeck.add(sandbagTreasureCard);
 			    			break;
 			    		case "R":
-			    			RiseWaterTreasureCard riseWaterTreasureCard = new RiseWaterTreasureCard(attributes[0]);
+			    			TreasureCard riseWaterTreasureCard = new RiseWaterTreasureCard(attributes[0]);
 			    			cardDeck.add(riseWaterTreasureCard);
 			    			break;
 			    	}
-				    
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
+			    	System.out.println(cardDeck);				
+		    	} 
+		    	catch (IOException e) {
 					e.printStackTrace();
 				}    
 			}
-		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
+		} 
+		catch (FileNotFoundException e1) {
 			e1.printStackTrace();
 		}				
 	}
