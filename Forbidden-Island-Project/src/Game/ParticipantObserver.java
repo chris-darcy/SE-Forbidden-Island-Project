8691fb4185ed9252552e0266dc3c37c4e128f88a
Aoibhe1998 <aoibhe.nithuineir-eanaigh@ucdconnect.ie>
2020-12-18 08:44:25 +0000

    Included printout for when a participant has drowned

---
+++ Forbidden-Island-Project/src/Game/ParticipantObserver.java
@@ -0,0 +1,21 @@
+package Game;
+
+import Observers.Subject;
+import Participant.Participant;
+
+public class ParticipantObserver extends Observer{
+	
+	private Subject subject;
+	
+	public ParticipantObserver(Subject subject) {
+		this.subject = subject;
+		this.subject.attach(this);
+	}
+	
+	@Override
+	public void update() {
+		//the participant has no where to move game has been lost
+		System.out.println(((Participant)this.subject).getName() + "the" + ((Participant)this.subject).getRoleName() + " has drowned!");
+		GameManager.getInstance().endGame(false);
+		}
+}
