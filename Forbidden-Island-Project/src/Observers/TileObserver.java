b90b819c16dffdf5551dc554a2ce71ca45a730ac
Aoibhe1998 <aoibhe.nithuineir-eanaigh@ucdconnect.ie>
2020-12-18 09:13:26 +0000

    Updated check for TileObserver

--- Forbidden-Island-Project/src/Observers/TileObserver.java
+++ Forbidden-Island-Project/src/Observers/TileObserver.java
@@ -1,6 +1,8 @@
 package Observers;
 
 import Board.Tile;
+import Board.TileStatus;
+import Board.TileType;
 import Game.GameManager;
 import Participant.Participant;
 
@@ -14,7 +16,11 @@ public class TileObserver extends Observer {
 	
 	@Override
 	public void update() {
-		System.out.println(((Tile)this.subject).getName() + " has sunk!");
-		GameManager.getInstance().endGame(false);
+		if(((Tile)this.subject).getTileType() != TileType.NORMAL &&   // double check the tile is a special tile
+		   ((Tile)this.subject).getTileType() != TileType.EMPTY  &&
+		   ((Tile)this.subject).getTileStatus() != TileStatus.SUNK) { // check the tile is sunk
+			System.out.println(((Tile)this.subject).getName() + " has sunk!");
+			GameManager.getInstance().endGame(false);                 // game over if this is true
+		}
 	}
 }
