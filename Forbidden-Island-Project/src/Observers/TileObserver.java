3c0b65c651d88a998dcf616fff487e10b441f349
Aoibhe1998 <aoibhe.nithuineir-eanaigh@ucdconnect.ie>
2020-12-18 08:46:47 +0000

    Created tile observer for when a special tile sinks

---
+++ Forbidden-Island-Project/src/Observers/TileObserver.java
@@ -0,0 +1,21 @@
+package Observers;
+
+import Board.Tile;
+import Game.GameManager;
+import Game.Observer;
+import Participant.Participant;
+
+public class TileObserver extends Observer {
+	private Subject subject;
+	
+	public TileObserver(Subject subject) {
+		this.subject = subject;
+		this.subject.attach(this);
+	}
+	
+	@Override
+	public void update() {
+		System.out.println(((Tile)this.subject).getName() + " has sunk!");
+		GameManager.getInstance().endGame(false);
+	}
+}
