c9a5cc5e26be1b37ed16dd508a406fe43afc076d
Aoibhe1998 <aoibhe.nithuineir-eanaigh@ucdconnect.ie>
2020-12-20 21:45:50 +0000

    Commented out notifyAllObservers() method as has been taken care ono longer required

--- Forbidden-Island-Project/src/Board/Tile.java
+++ Forbidden-Island-Project/src/Board/Tile.java
@@ -60,7 +60,7 @@ public class Tile extends Subject{
 			GM.updateSpecialTileStatus(this);
 		}
 		this.tileStatus = tileStatus;
-		notifyAllObservers();
+//		notifyAllObservers();
 	}
 	
 	// get the type of the tile
