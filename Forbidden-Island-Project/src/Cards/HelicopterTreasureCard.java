d46ecae9ef6fe8031bf6175d3614aacc5e9624fe
Aoibhe1998 <aoibhe.nithuineir-eanaigh@ucdconnect.ie>
2020-12-21 00:25:43 +0000

    removed unecessary import

--- Forbidden-Island-Project/src/Cards/HelicopterTreasureCard.java
+++ Forbidden-Island-Project/src/Cards/HelicopterTreasureCard.java
@@ -1,15 +1,9 @@
 package Cards;
 
-import Participant.Participant;
-
 public class HelicopterTreasureCard extends Card{
 	
 	public HelicopterTreasureCard(String name) {
 		super(name);
 	}
-	// one or more users can be moved, this will be called for each user to be moved
-	public static void helicopterLift(Participant toBeMoved, int location) {  
-		toBeMoved.setLocation(location); // relocated the chosen user 
-		
-	}
+	
 }
