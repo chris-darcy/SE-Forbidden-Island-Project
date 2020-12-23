89f865d8767fdc8a832e116138a0b0a467b1f466
Aoibhe1998 <aoibhe.nithuineir-eanaigh@ucdconnect.ie>
2020-12-23 12:34:05 +0000

    Comment added

--- Forbidden-Island-Project/src/Main/Main.java
+++ Forbidden-Island-Project/src/Main/Main.java
@@ -1,7 +1,9 @@
 package Main;
 
 import Game.GameManager;
-
+//
+// Class run to play the Forbidden Island Game!
+//
 public class Main {
 	public static void main(String args[]) {
 		GameManager GM = GameManager.getInstance();
